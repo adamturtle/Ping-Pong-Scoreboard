2874a41fe77c9caf2c5c1bd0a2b93315869fcc64
Michael Mottola <mikemottola@gmail.com>
2014-02-01 10:13:38 -0500

    Updated Port number for deploying to heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express')
-  , port = 3000
+  , port = process.env.PORT || 3000
   , app = express()
   , jade = require('jade')
   , server = require('http').createServer(app)
